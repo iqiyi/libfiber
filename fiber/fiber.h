9d2a95d9421f944e4864e05f105bec9102fb1a81
zsx <zhengshuxin@gmail.com>
2018-04-25 14:44:32 +0800

    add xcode project

---
+++ fiber/fiber.h
@@ -0,0 +1,23 @@
+/*
+ *  fiber.h
+ *  fiber
+ *
+ *  Created by zsx on 18/4/25.
+ *  Copyright (c) 2018年 zsx. All rights reserved.
+ *
+ */
+
+#ifndef fiber_
+#define fiber_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class fiber
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
