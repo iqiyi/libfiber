023b2fe603015b570786a019ed1cd082569ec06c
ruki <waruqi@gmail.com>
2018-07-10 16:22:53 +0800

    update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -20,3 +20,51 @@ target("fiber")
     -- add flags
     add_cxflags("-std=gnu99")
 
+    -- add defines
+    add_defines("USE_JMP")
+
+-- samples: client
+target("client")
+    set_kind("binary")
+    add_deps("fiber")
+    add_files("samples/patch.c")
+    add_files("samples/client/*.c")
+
+-- samples: server
+target("server")
+    set_kind("binary")
+    add_deps("fiber")
+    add_files("samples/patch.c")
+    add_files("samples/server/*.c")
+
+-- benchmark: libco_server
+target("libco_server")
+    set_default(false)
+    set_kind("binary")
+    add_deps("fiber")
+    add_files("benchmark/patch.cpp")
+    add_files("benchmark/libco/*.cpp")
+
+-- benchmark: libgo_server
+target("libgo_server")
+    set_default(false)
+    set_kind("binary")
+    add_deps("fiber")
+    add_files("benchmark/patch.cpp")
+    add_files("benchmark/libgo/*.cpp")
+
+-- benchmark: libmill_server
+target("libmill_server")
+    set_default(false)
+    set_kind("binary")
+    add_deps("fiber")
+    add_files("benchmark/patch.c")
+    add_files("benchmark/libmill/*.c")
+
+-- benchmark: tbox_server
+target("tbox_server")
+    set_default(false)
+    set_kind("binary")
+    add_deps("fiber")
+    add_files("benchmark/patch.c")
+    add_files("benchmark/tbox/*.c")
